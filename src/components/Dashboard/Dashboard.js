52fabce166c3b0f81faea0c159487b3695021c32
mowuj <mowuj92@gmail.com>
2022-04-04 14:56:39 +0600

    emplement lineCahart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MyLineChart from '../MyLineChart/MyLineChart';
 
 const Dashboard = () => {
     return (
-        <div>
+        <div className='chart'>
             <h2>This is Dashboard</h2>
+            <MyLineChart></MyLineChart>
         </div>
     );
 };
